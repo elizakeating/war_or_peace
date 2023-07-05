76a378f07dab2f96ae2ced3a173983a878ab6b47
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-04 21:48:36 -0600

    Add remove card and check deck tests to deck spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -61,6 +61,30 @@ RSpec.describe Deck do
 
         expect(deck.percent_high_ranking).to eq(66.67)
     end
+
+    it "removes card from deck" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        
+        deck = Deck.new(cards)
+
+        expect(deck.remove_card).to eq(card1)
+    end
+
+    it "deck has two cards" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card3 = Card.new(:heart, 'Ace', 14)
+        cards = [card1, card2, card3]
+        
+        deck = Deck.new(cards)
+
+        deck.remove_card
+
+        expect(deck.cards). to eq([card2, card3])
+    end
     
 end
 
