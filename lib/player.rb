66e5a67a4643448af247cf67954235966d5946ad
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-05 11:50:41 -0600

    Add has lost? method to player class

--- lib/player.rb
+++ lib/player.rb
@@ -7,4 +7,25 @@ class Player
         @deck = deck
     end
 
+    def has_lost?
+        if deck.cards.length > 0
+            false
+        else
+            true
+        end
+    end
+
 end
+
+# require './lib/card'
+# require './lib/deck'
+# require './lib/player'
+
+# card1 = Card.new(:diamond, 'Queen', 12)
+# card2 = Card.new(:spade, '3', 3)
+# card3 = Card.new(:heart, 'Ace', 14)
+
+# deck = Deck.new([card1, card2, card3])
+
+# player = Player.new('Clarisa', deck)
+
