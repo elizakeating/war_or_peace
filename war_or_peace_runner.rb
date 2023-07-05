6bbac217f65eab58e877546baa555690755a0372
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-05 16:36:58 -0600

    Add beginning statement and starting prompt to runner

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,6 +2,7 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game'
 
 
 suits = [:heart, :spade, :diamond, :club]
@@ -10,6 +11,40 @@ full_deck = []
 suits.each do |suit| #since each suit will have all the values and ranks, we want to do this loop for each of them
     value.length.times do |i| #we want to loop this the amount of cards in each suit, which is determined by the length of the value array
             full_deck << Card.new(suit, value[i], i+2) #this code is run 13 times for each suit, the i + 2 is because we start at 2 for rank and i starts at 0
+    end
+end
+
+shuffled_deck = full_deck.shuffle
+
+deck1_cards = []
+deck2_cards = []
+
+26.times do
+    deck1_cards << shuffled_deck.shift
+end
+26.times do 
+    deck2_cards << shuffled_deck.shift
+end
+
+deck1 = Deck.new(deck1_cards)
+deck2 = Deck.new(deck2_cards)
+
+player1 = Player.new("Megan", deck1)
+player2 = Player.new("Aurora", deck2)
+
+game = Game.new
+
+puts "Welcome to War! (or Peace) This game will be played with 52 cards.\nThe players today are #{player1.name} and #{player2.name}.\nType 'GO' to start the game!\n------------------------------------------------------"
+
+word = gets.chomp
+
+loop do
+    if word == "GO"
+        game.start
+        break
+    else
+        p "Incorrect, try again!"
+        word = gets.chomp
     end
 end
     
